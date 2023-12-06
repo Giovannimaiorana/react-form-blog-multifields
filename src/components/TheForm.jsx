49c0f2f1e6c131bbde40c17554a82ca227de02ff
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-12-06 16:42:09 +0100

    esercizio base concluso

--- src/components/TheForm.jsx
+++ src/components/TheForm.jsx
@@ -3,44 +3,71 @@ import { v4 as uuidv4 } from "uuid";
 import style from "../css/modules/TheForm.module.css";
 
 export default function TheForm() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    image: "",
+    category: "",
+    tags: [],
+    isVisible: false,
+  });
+
   const [articles, setArticles] = useState([]);
   const [editingId, setEditingId] = useState(null);
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleDescriptionChange = (e) => {
-    setDescription(e.target.value);
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => {
+      if (type === "checkbox" && name === "tags") {
+        if (checked) {
+          return {
+            ...prevData,
+            tags: [...prevData.tags, value],
+          };
+        } else {
+          return {
+            ...prevData,
+            tags: prevData.tags.filter((tag) => tag !== value),
+          };
+        }
+      } else {
+        return {
+          ...prevData,
+          [name]: type === "checkbox" ? checked : value,
+        };
+      }
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const { title, description, image, category, tags, isVisible } = formData;
+
     if (title.trim() !== "" && description.trim() !== "") {
-      if (editingId !== null) {
-        setArticles((prevArticles) =>
-          prevArticles.map((article) =>
-            article.id === editingId
-              ? { ...article, title, description }
-              : article
-          )
-        );
-        setEditingId(null);
-      } else {
-        setArticles([...articles, { id: uuidv4(), title, description }]);
-      }
-      setTitle("");
-      setDescription("");
+      const updatedArticles =
+        editingId !== null
+          ? articles.map((article) =>
+              article.id === editingId ? { ...article, ...formData } : article
+            )
+          : [...articles, { id: uuidv4(), ...formData }];
+
+      setArticles(updatedArticles);
+      setEditingId(null);
+      setFormData({
+        title: "",
+        description: "",
+        image: "",
+        category: "",
+        tags: [],
+        isVisible: false,
+      });
     }
   };
 
   const handleEdit = (id) => {
     const articleToEdit = articles.find((article) => article.id === id);
     if (articleToEdit) {
-      setTitle(articleToEdit.title);
-      setDescription(articleToEdit.description);
+      setFormData({ ...articleToEdit });
       setEditingId(id);
     }
   };
@@ -60,8 +87,9 @@ export default function TheForm() {
         <input
           type="text"
           id="title"
-          value={title}
-          onChange={handleTitleChange}
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
           placeholder="Inserisci Titolo Post"
         />
 
@@ -73,38 +101,109 @@ export default function TheForm() {
         <input
           id="description"
           type="text"
-          value={description}
-          onChange={handleDescriptionChange}
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
           placeholder="Inserisci Descrizione"
         />
+
+        <label htmlFor="image">
+          {" "}
+          {editingId !== null ? "Modifica Immagine" : "Inserisci Immagine"}
+        </label>
+        <input
+          type="text"
+          id="image"
+          name="image"
+          value={formData.image}
+          onChange={handleChange}
+          placeholder="URL dell'immagine"
+        />
+
+        <label htmlFor="category">Categoria</label>
+        <select
+          id="category"
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option disabled value="">
+            Seleziona una categoria
+          </option>
+          <option value="categoria1">Categoria 1</option>
+          <option value="categoria2">Categoria 2</option>
+          {/* Aggiungi altre opzioni secondo le tue esigenze */}
+        </select>
+
+        <label>Tags</label>
+        <div>
+          <input
+            type="checkbox"
+            id="tag1"
+            name="tags"
+            value="tag1"
+            checked={formData.tags.includes("tag1")}
+            onChange={handleChange}
+          />
+          <label htmlFor="tag1">Tag 1</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            id="tag2"
+            name="tags"
+            value="tag2"
+            checked={formData.tags.includes("tag2")}
+            onChange={handleChange}
+          />
+          <label htmlFor="tag2">Tag 2</label>
+        </div>
+
+        <label>
+          <input
+            type="checkbox"
+            id="isVisible"
+            name="isVisible"
+            checked={formData.isVisible}
+            onChange={handleChange}
+          />
+          Pubblica
+        </label>
+
         <button className={style.buttonSub} type="submit">
           {editingId !== null ? "Modifica" : "Aggiungi"}
         </button>
       </form>
-      <h1>Post</h1>
 
+      <h1>Post</h1>
       <div className={style.postStyle}>
         <ul>
-          {articles.map((article) => (
-            <li key={article.id}>
-              <h1>{article.title}</h1>
-              <p>{article.description}</p>
-              <div>
-                <button
-                  className={style.buttonEdit}
-                  onClick={() => handleEdit(article.id)}
-                >
-                  Modifica
-                </button>
-                <button
-                  className={style.buttonDelete}
-                  onClick={() => handleDelete(article.id)}
-                >
-                  Elimina
-                </button>
-              </div>
-            </li>
-          ))}
+          {articles
+            .filter((article) => article.isVisible)
+            .map((article) => (
+              <li key={article.id}>
+                <h1>{article.title}</h1>
+                <img className={style.imgStyle} src={article.image} alt="" />
+                <p>{article.description}</p>
+                <p>Categoria: {article.category}</p>
+                <p>Tags: {article.tags.join(", ")}</p>
+                <p>Pubblicato: {article.isVisible ? "Sì" : "No"}</p>
+                <div>
+                  <button
+                    className={style.buttonEdit}
+                    onClick={() => handleEdit(article.id)}
+                  >
+                    Modifica
+                  </button>
+                  <button
+                    className={style.buttonDelete}
+                    onClick={() => handleDelete(article.id)}
+                  >
+                    Elimina
+                  </button>
+                </div>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
